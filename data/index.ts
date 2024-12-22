0c895e060ff150f0f3d13cb9ace66affeeb1d0fd
YogeshMahajanGit <mahajanyogesh443@gmail.com>
2024-12-22 05:12:19 -0800

    add all files

---
+++ data/index.ts
@@ -0,0 +1,130 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Collaborative problem solver with a proactive approach to development.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Adaptable and efficient.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Mastery in MERN stack and modern web technologies.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title:
+      "Full-stack developer passionate about creating impactful solutions.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title:
+      "Currently enhancing Backend development skills and exploring Docker.",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Excited to collaborate on groundbreaking projects.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Echo - Chat App",
+    des: "A feature-rich chat application with real-time messaging, group chats, and socket integration. Built for seamless communication.",
+    img: "/echo.png",
+    iconLists: [
+      "/re.svg",
+      "/socket.io.svg",
+      "/redux.svg",
+      "/cloud.svg",
+      "/dock.svg",
+    ],
+    link: "https://echo-client-v7ii.onrender.com/",
+  },
+  {
+    id: 2,
+    title: "Blogger",
+    des: "A modern blogging platform with user authentication, image uploads to AWS, and rich text editing capabilities.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/ts.svg", "/tail.svg", "/ts.svg"],
+    link: "https://github.com/YogeshMahajanGit/blogger",
+  },
+  {
+    id: 3,
+    title: "Pizza - Online Ordering System",
+    des: "An intuitive pizza ordering app with geolocation-based address autofill, cart management, and real-time order tracking.",
+    img: "/pizza.png",
+    iconLists: ["/re.svg", "/tail.svg", "/redux.svg"],
+    link: "https://pizzacom-mahajanyogesh443-gmailcoms-projects.vercel.app",
+  },
+  {
+    id: 4,
+    title: "Crypto Analysis Tool",
+    des: "A comprehensive tool for analyzing cryptocurrency trends and data, featuring advanced visualizations and real-time updates.",
+    img: "",
+    iconLists: ["/next.svg", "/tail.svg"],
+    link: "https://github.com/YogeshMahajanGit/CryptoWord",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/YogeshMahajanGit",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/dev-yogesh-mahajan/",
+  },
+  {
+    id: 3,
+    img: "/twit.svg",
+    link: "https://x.com/MahajanYog12729",
+  },
+];
